de6517bf0d7f083e0382e0a47a9b2ffd52349350
op <piganiolivia@gmail.com>
2023-12-03 17:38:10 +0100

    question entity is done

--- src/main/java/com/project/quizz/controllers/QuestionController.java
+++ src/main/java/com/project/quizz/controllers/QuestionController.java
@@ -1,5 +1,7 @@
-package com.project.quizz;
+package com.project.quizz.controllers;
 
+import com.project.quizz.services.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("question")
 public class QuestionController {
 
+    @Autowired
+    QuestionService questionService;
+
 
     @GetMapping("allQuestions")
     public String getAllQuestions() {
-        return "test";
+        return questionService.getAllQUestions();
     }
 
 }
---
+++ src/main/java/com/project/quizz/services/QuestionService.java
@@ -0,0 +1,12 @@
+package com.project.quizz.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionService {
+
+
+
+
+}
